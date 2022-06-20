4f40a32d48ab9fc1e5cfd8bd998d28fa2e51e8e6
Theo Walker <theo.ca.walker@gmail.com>
2022-06-20 18:34:42 -0400

    speed up pandas updates (?) and fix bugs

--- rangeindex/idx_pandas.py
+++ rangeindex/idx_pandas.py
@@ -1,5 +1,6 @@
 from typing import *
 
+import numpy as np
 import pandas as pd
 
 from rangeindex.constants import *
@@ -33,6 +34,7 @@ class PandasIndex:
         new_df[PYOBJ_ID_COL] = [ptr]
         new_df[PYOBJ_COL] = [obj]
         self.df = pd.concat([self.df, new_df])
+        self.df.sort_values(PYOBJ_ID_COL, inplace=True)
 
     def add_many(self, objs: List[Any]):
         """Add a collection of objects to the index."""
@@ -55,9 +57,10 @@ class PandasIndex:
                 for field, dtype in self.fields.items()
             }
         )
-        new_df[PYOBJ_ID_COL] = list(new_objs.keys())
+        new_df[PYOBJ_ID_COL] = np.array(list(new_objs.keys()), dtype='uint64')
         new_df[PYOBJ_COL] = list(new_objs.values())
         self.df = pd.concat([self.df, new_df])
+        self.df.sort_values(PYOBJ_ID_COL, inplace=True)
 
     def find(self, where: Optional[str] = None) -> List:
         """Find Python objects that match the query constraints."""
@@ -66,24 +69,45 @@ class PandasIndex:
         return self.df.query(where)[PYOBJ_COL].to_list()
 
     def remove(self, obj: Any):
-        """Remove a single object from the index. Slow operation (requires table scan + rebuilding parts of table)."""
-        i = self.df[self.df[PYOBJ_ID_COL] == id(obj)].index[0]
-        self.df.drop(i, inplace=True)
+        """Remove a single object from the index. Slow operation (requires rebuilding parts of table)."""
+        ptr = id(obj)
+        i = self._find_idx(ptr)
+        if i == -1:
+            raise KeyError(f'Could not find object with id: {ptr}')
+        else:
+            self.df.drop(index=self.df.index[i], inplace=True)
 
     def update(self, obj: Any, updates: Dict[str, Any]):
-        """Update a single object in the index. Somewhat slow operation (requires table scan)."""
-        i = self.df[self.df[PYOBJ_ID_COL] == id(obj)].index[0]
-        for field, new_value in updates.items():
-            if field in self.fields:
-                self.df.at[i, field] = new_value
-            # apply changes to the obj as well
-            setattr(obj, field, new_value)
+        """Update a single object in the index. Fast - it's O(log(n)) since table is sorted by object id."""
+        ptr = id(obj)
+        i = self._find_idx(ptr)
+        if i == -1:
+            raise KeyError(f'Could not find object with id: {ptr}')
+        else:
+            for field, new_value in updates.items():
+                if field in self.fields:
+                    field_loc = self.df.columns.get_loc(field)
+                    self.df.iloc[i, field_loc] = new_value
+                # apply changes to the obj as well
+                setattr(obj, field, new_value)
+
+    def _find_idx(self, ptr: int) -> int:
+        """
+        Look up a pointer in {PYOBJ_ID_COL} and returns its numeric position ("iloc" in pandas terms).
+
+        Returns -1 if not found.
+        """
+        i = self.df[PYOBJ_ID_COL].searchsorted(ptr)
+        pyobj_col_loc = self.df.columns.get_loc(PYOBJ_ID_COL)
+        if self.df.iloc[i, pyobj_col_loc] == ptr:
+            return i
+        return -1
 
     def __len__(self) -> int:
         return len(self.df)
 
     def __contains__(self, obj) -> bool:
-        return id(obj) in self.df[PYOBJ_ID_COL]
+        return self._find_idx(id(obj)) != -1
 
     def __iter__(self):
         return iter(self.df[PYOBJ_COL])
