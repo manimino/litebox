8350674f2b660b0e31e05fdc3901b4c15702fc4d
Theo Walker <theo.ca.walker@gmail.com>
2022-06-23 08:45:04 -0400

    type() -> isinstance()

--- rangeindex/utils.py
+++ rangeindex/utils.py
@@ -3,7 +3,7 @@ from typing import Any
 
 def get_field(obj: Any, field: str):
     """Get a field from a Python object (class, dataclass, namedtuple) or from a dict"""
-    if type(obj) is dict:
+    if isinstance(obj, dict):
         return obj.get(field, None)
     else:
         return getattr(obj, field, None)
@@ -11,7 +11,7 @@ def get_field(obj: Any, field: str):
 
 def set_field(obj: Any, field: str, new_value: Any):
     """Set a field on a Python object (class, dataclass, namedtuple) or from a dict"""
-    if type(obj) is dict:
+    if isinstance(obj, dict):
         obj[field] = new_value
     else:
         setattr(obj, field, new_value)
